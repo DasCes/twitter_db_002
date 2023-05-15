c9f1ae375ae73c511a6603887b31e9e3ef13623e
dario_ceni <dario01ceni@gmail.com>
2023-05-15 22:40:50 +0200

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from google.cloud import firestore
+
+
+# qui ci stiamo autenticando a Firestore con la chiave json scaricata e inserita nel progetto
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
+
+posts_ref = db.collection("tweets")
+
+# stampiamo tutto il db con un ciclo
+for doc in posts_ref.stream():
+    st.write("the text is: ", doc.text)
