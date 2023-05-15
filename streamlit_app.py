f178a9e1ce9d756f32cd577d1b08316a44774d31
dario_ceni <dario01ceni@gmail.com>
2023-05-15 23:46:43 +0200

    index fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from google.cloud import firestore
 import pandas as pd
+import schedule
+import time
+
 
 # qui ci stiamo autenticando a Firestore con la chiave json scaricata e inserita nel progetto
 db = firestore.Client.from_service_account_json("firestore-key.json")
@@ -12,21 +15,31 @@ db_ref = db.collection("tweeets")
 df = pd.read_csv(f'data/data.csv', index_col=[0])
 df = df.head(20)
 
-for index, row in df.iterrows():
-    doc_ref = db_ref.document("i" + str(index))
-    doc = doc_ref.get()
-
-    if not doc.exists:
-        doc_ref.set({
-            'id': index,
-            'text': row['text'],
-            'created_at': row['created_at'],
-            'text_clean_IT': row['text_clean_IT']
-        })
-
-
 
 # stampiamo tutto il db con un ciclo
 for doc in db_ref.stream():
     st.write("the id is: ", doc.id)
     st.write("contents of db: ", doc.to_dict())
+
+
+WAIT_SECONDS = 5
+def aggiungiTweetOgniNSecondi():
+    for index, row in df.iterrows():
+        doc_ref = db_ref.document("i" + str(index))
+        doc = doc_ref.get()
+
+        if not doc.exists:
+            doc_ref.set({
+                'id': index,
+                'text': row['text'],
+                'created_at': row['created_at'],
+                'text_clean_IT': row['text_clean_IT']
+            })
+
+schedule.every(WAIT_SECONDS).seconds.do(aggiungiTweetOgniNSecondi)
+
+
+
+while True:
+        schedule.run_pending()
+        time.sleep(1)
